954e304a4e3b3085616421a5dd1f171acc3812c3
Juan Sifuentes Fernandez <89866333+JuanSifuentesF@users.noreply.github.com>
2024-11-22 17:54:16 -0500

    actualizacion_1

---
+++ Tema01/variables.js
@@ -0,0 +1,13 @@
+
+    /*
+        Tipos de Datos:
+        undefined, null, boolean, string, number, object
+    */
+   var miVariable = "freeCodeCamp";
+
+   miVariable = 16;
+   console.log(miVariable)
+
+    
+
+   
