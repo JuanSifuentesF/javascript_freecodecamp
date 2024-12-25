feafb5a1772544abb0c4401083e6988db3af28ab
Juan Sifuentes Fernandez <89866333+JuanSifuentesF@users.noreply.github.com>
2024-12-24 19:32:05 -0500

    Actualizacion 24-12_

--- 04_EstructuraDeDatos/04_06_BlackJack.js
+++ 04_EstructuraDeDatos/04_06_BlackJack.js
@@ -40,3 +40,34 @@ El conteo actual y la decision del jugador
 
 var conteo = 0;
 
+function contarCartas(carta) {
+    var decision;
+    switch (carta) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            conteo++;
+            break;
+        case 7:
+        case 8:
+        case 9:
+            conteo += 0;
+            break;
+        case 10:
+        case 'J':
+        case 'Q':
+        case 'K':
+        case 'A':
+            conteo--;
+            break;
+    }
+    if (conteo > 0) {
+        decision = "Apostar";
+    } else {
+        decision = "Esperar";
+    }
+
+    return conteo + " " + decision;
+}
